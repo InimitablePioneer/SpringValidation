9ad0091bc3ced9228ab3a9d58ec1aeb749e7d5d8
InimitablePioneer <seungwonpioneer@gmail.com>
2024-05-21 16:00:29 +0900

    May 21 at work via WIN

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,18 +12,20 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price*_this.quantity >= 10000", message = "만원 넘겨야됨 ㅇㅇ")
 public class Item {
 
-    @NotNull // 수정 요구 사항 추가
+    //@NotNull // 수정 요구 사항 추가
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class,UpdateCheck.class})
     private Integer price;
 
-    @NotNull
+    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
     //@Max(9999) //수정 요구 사항 추가
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
@@ -33,5 +35,9 @@ public class Item {
         this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
